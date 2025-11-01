e7a62c4ce856472b19cf2dd115e1302104a93c79
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2025-11-01 11:41:19 +0530

    first commit

---
+++ 2.Operators/A1.cpp
@@ -0,0 +1,10 @@
+// Q1. predict output of cout<<(bool)3+2)
+// Q2. predict output of cout<<(23.5 + 2 + 'A')
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<((bool)3+2)<<endl;
+    cout<< (23.5 + 2 + 'A')<<endl;
+}
