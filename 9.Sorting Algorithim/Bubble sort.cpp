f257eb07ba0099fc83c42025fe43b961a7653053
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2025-12-08 12:37:50 +0530

    first commit

---
+++ 9.Sorting Algorithim/Bubble sort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void printarr(int arr[], int n) {
+    for (int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubblesort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[5] = {5, 4, 3, 2, 1};
+
+    bubblesort(arr, 5);
+
+    printarr(arr, 5);
+
+    return 0;
+}
