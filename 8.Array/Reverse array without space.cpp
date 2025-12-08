d6e768bb19b77b7449ce1746ceac82197cbe608a
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2025-12-08 12:24:01 +0530

    first commit

---
+++ 8.Array/Reverse array without space.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int printarr(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+
+int main() {
+    int arr[]={ 5,4,3,2,10};
+    int n=sizeof(arr)/sizeof(int);
+
+    int start=0, end= n-1;
+
+    while (start <end) {
+        int temp =arr[start];
+        arr[start]=arr[end];
+        arr[end]= temp;
+        start ++;
+        end --;
+    }
+    printarr(arr,n);
+    return 0;
+}
