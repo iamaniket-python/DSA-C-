7c9b0a4af497e7cd395ff85d5ab296c3e0782ee6
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2025-12-09 10:02:44 +0530

    first commit

--- 8.Array/Twice a number.cpp
+++ 8.Array/Twice a number.cpp
@@ -10,3 +10,31 @@
 // nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
 // Output:
 // true
+
+#include<iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool duplicatenumber(int arr[], int n) {
+    sort(arr, arr + n);
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr[i - 1]) {
+            return true;
+        }
+    }
+    return false;
+}
+int main() {
+    int arr[] = {1,2,2,3,3,4,4,5,5};
+    int n = sizeof(arr) / sizeof(int);
+
+    if(duplicatenumber(arr, n)) {
+        cout << "Duplicate exists";
+    }
+    else {
+        cout << "No duplicates";
+    }
+
+    return 0;
+}
