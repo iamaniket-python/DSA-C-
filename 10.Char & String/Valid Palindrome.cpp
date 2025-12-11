7ec46697da3b0a686d3c581f497c67f0b0285615
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2025-12-11 15:15:27 +0530

    first commit

---
+++ 10.Char & String/Valid Palindrome.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+bool ispalidrome(char str[], int n) {
+    int st=0;
+    int end=n-1;
+    while (st<end) {
+        if (str[st++] != str[end--]) {
+            cout<<"not valid";
+            return false;
+        }
+    }
+    cout<<"Valid";
+    return true;
+}
+int main() {
+    char word[20];
+    cout<<"enter the word"<<endl;
+    cin>>word;
+
+    ispalidrome(word,strlen(word));
+    return 0;
+}
