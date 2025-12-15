e83f672a577e75208099bc1803c7bdb080de8cbf
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2025-12-15 10:42:14 +0530

    first commit

---
+++ 11.Vectors/1D Array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int size;
+    cout<<"Enter a number =";
+    cin>>size;
+
+    int *arr =new int[size];
+
+    int x=1;
+    for (int i=0; i<size; i++) {
+        arr[i]=x;
+        cout<<arr[i]<<" \n";
+        x++;
+    }
+    cout<<endl;
+    return 0;
+}
