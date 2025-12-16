b93fe189206c1bda5e35ae269a4e5537925a769b
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2025-12-16 14:51:04 +0530

    first commit

---
+++ 12.Bit Manipulation/CLear Last Bits.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void CLearlastbit(int num, int i) {
+    int bitmask=(~0)<<i;
+    num =num & bitmask;
+
+    cout<<num <<endl;
+}
+int main() {
+    CLearlastbit(15,2);
+    return 0;
+}
