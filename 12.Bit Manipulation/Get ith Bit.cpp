4b71e120b1c4ca85184e9503a0f12fe07ac0b97e
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2025-12-16 14:39:22 +0530

    first commit

---
+++ 12.Bit Manipulation/Get ith Bit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int getBit(int num, int i) {
+    int bitMask=1 <<i;
+    if (!(num & bitMask)) {
+        return 0;
+    }else {
+        return 1;
+    }
+}
+int main() {
+    cout<<getBit(6,2);
+    return 0;
+}
