b62f32d89add992026b72d7905b3c5974e8bd671
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-01-17 15:59:14 +0530

    Merged remote changes

--- 6.Functions/Reverse.cpp
+++ 6.Functions/Reverse.cpp
@@ -3,22 +3,16 @@ using namespace std;
 
 string reverseString(string s) {
     string rev = "";
-
     for (int i = s.length() - 1; i >= 0; i--) {
         rev += s[i];
     }
-
     return rev;
 }
-
 int main() {
     string str;
-
     cout << "Enter a string: ";
     cin >> str;
-
     string result = reverseString(str);
     cout << "Reversed: " << result;
-
     return 0;
 }
