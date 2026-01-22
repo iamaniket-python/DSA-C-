2532005f481e978ac2ef3370c585982934d48300
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-01-22 17:11:33 +0530

    Merged remote changes

---
+++ 6.Functions/Frequency of Number.cpp
@@ -0,0 +1,11 @@
+// Write a function that counts how many times each digit appears in a number.
+#include<iostream>
+using namespace std;
+
+int Digitappear(int arr[], int size) {
+
+    int count=0;
+    for (int i=0; i<size; i++) {
+        if (arr[i] )
+    }
+}
