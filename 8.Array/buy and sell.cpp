b4dfcb07585f51545b4f9a6b6e744be7b1b2c1f2
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-01-30 20:18:14 +0530

    cpp

---
+++ 8.Array/buy and sell.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void maxProfit(int *prices, int n){
+    int bestBuy[100000];
+    bestBuy[0]=INT_MAX;
+
+    for(int i=1; i<n; i++){
+        bestBuy[i] =min(bestBuy[i-1], prices[i-1]);
+    }
+    int maxProfit=0;
+    for(int i=0; i<n; i++){
+        int currProfit=prices[i]- bestBuy[i]
+        maxProfit =max(maxProfit, currProfit);
+    }
+    cout<<"Max Profit"<<maxProfit<<endl;
+}
+
+
+
+
+int main() {
+    int prices[6]={2,3,4,1,4,-6}
+    int n= sizeof(prices)/ sizeof(int);
+    maxProfit(prices,n);
+    return 0;
+};
