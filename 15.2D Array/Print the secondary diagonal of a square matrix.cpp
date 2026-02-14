05c4a1ffdc6516c7cf1709f91dfc6ef317bb48b9
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-02-14 19:50:46 +0530

    cpp

---
+++ 15.2D Array/Print the secondary diagonal of a square matrix.cpp
@@ -0,0 +1,18 @@
+// Print the secondary diagonal of a square matrix.
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[3][4]={
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+    };
+    int n=4;
+    cout<<"Main Diagonal=";
+    for (int i=0; i<n; i++) {
+        cout<<arr[i][n-1-i]<<" ";
+    }
+    return 0;
+}
+
