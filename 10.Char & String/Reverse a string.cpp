bde120a5861ab65bb6e65ceb03cd80f7b09e2617
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-02-20 19:37:11 +0530

    cpp

---
+++ 10.Char & String/Reverse a string.cpp
@@ -0,0 +1,21 @@
+// Reverse a string
+#include <iostream>
+using namespace std;
+
+void reverseString(string &str) {
+    int start = 0;
+    int end = str.length() - 1;
+
+    while (start < end) {
+        swap(str[start], str[end]);
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    string str = "Hello";
+    reverseString(str);
+    cout << str;
+    return 0;
+}
