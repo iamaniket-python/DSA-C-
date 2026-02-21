4ee577fb460ac8f58ef862b7c78b48a7450da6b6
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-02-21 21:03:09 +0530

    cpp

---
+++ 10.Char & String/Find length of string without using.cpp
@@ -0,0 +1,12 @@
+// Find length of string without using strlen()
+#include<iostream>
+using namespace std;
+
+int main() {
+    string str="Aniket srivastava";
+    int count =0;
+    for (int i=0; str[i] != '\0'; i++) {
+       count++;
+    }
+    cout<<count<<endl;
+}
