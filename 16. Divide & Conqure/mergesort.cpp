ce7a53e6dab14acf4a2ef5f6d5dfa18db484dc33
Aniket Shrivastava <aniketsrivastava57@gmail.com>
2026-03-07 19:33:39 +0530

    first commit

---
+++ 16. Divide & Conqure/mergesort.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void merge(int arr[], int si, int mid, int ei) {
+    vector<int> temp;
+    int i = si;
+    int j = mid + 1;
+    while (i <= mid && j <= ei) {
+        if (arr[i] <= arr[j]) {
+            temp.push_back(arr[i]);
+            i++;
+        } else {
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+    while (i <= mid) {
+        temp.push_back(arr[i++]);
+    }
+
+    while (j <= ei) {
+        temp.push_back(arr[j++]);
+    }
+
+    for (int idx = si, x = 0; idx <= ei; idx++) {
+        arr[idx] = temp[x++];
+    }
+}
+void mergesort(int arr[], int si, int ei) {
+    if (si >= ei) return;
+
+    int mid = si + (ei - si) / 2;
+
+    mergesort(arr, si, mid);
+    mergesort(arr, mid + 1, ei);
+    merge(arr, si, mid, ei);
+}
+void printarr(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main() {
+    int arr[] = {6,3,5,7,8,2,9};
+    int n = 7;
+    mergesort(arr, 0, n - 1);
+    printarr(arr, n);
+    return 0;
+};
